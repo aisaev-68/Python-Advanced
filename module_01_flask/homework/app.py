4889455cf3b14dfa362a1bc53d2d00d05bfafa4a
Рамазан Айсаев <aisaev-68@yandex.ru>
2022-04-30 13:25:04 +0300

    ok

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,29 +7,35 @@ app = Flask(__name__)
 
 @app.route('/hello_world')
 def test_function():
-   pass
+   return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def test_cars():
+   return 'Chevrolet, Renault, Ford, Lada'
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def test_cats():
+   return random.choice(['Корниш рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мэйн-Кун', 'Манчкин'])
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def test_time():
+   return f'Точное время: {datetime.datetime.now()}'
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def test_future():
+   today = datetime.datetime.now()
+   current_time_after_hour = today + datetime.timedelta(hours=1)
+   return f'Точное время через час будет {current_time_after_hour}'
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def test_word():
+   with open("war_and_peace.txt", "r", encoding='utf-8') as file:
+      txt = file.read()
+      words = list(map(str, txt.split()))
+      return f'Случайное слово из книги Война и Мир Льва Толстого: {random.choice(words)}'
+
--- module_01_flask/materials/first_application/app.py
+++ module_01_flask/materials/first_application/app.py
@@ -2,8 +2,22 @@ import datetime
 from flask import Flask
 
 app = Flask(__name__)
+count = 0
+
 
 @app.route('/test')
 def test_function():
-   return 'Это тестовая страничка, ответ сгенерирован в %s' % \
-                     datetime.datetime.now().utcnow()
+    return 'Это тестовая страничка, ответ сгенерирован в %s' % \
+           datetime.datetime.now().utcnow()
+
+
+@app.route('/hello/world')
+def test_hello():
+    return 'Hello, world!'
+
+
+@app.route('/counter')
+def test_count():
+    global count
+    count += 1
+    return f'Количество посещений сайта: {count}'
