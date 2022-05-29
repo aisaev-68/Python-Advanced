613b83db6635a2c1437a94221f0941628501e2d6
ramazan_aisaev <aisaev-68@yandex.ru>
2022-05-29 22:08:54 +0300

    ok

--- module_07_logging_part_2/homework/http_handler.py
+++ module_07_logging_part_2/homework/http_handler.py
@@ -27,7 +27,7 @@ logging_config = {
             "formatter": "formatter",
             "host": 'http://127.0.0.1:5000',
             'url': '/',
-            'method': 'GET'
+            'method': 'POST'
         },
     },
     'formatters': {
